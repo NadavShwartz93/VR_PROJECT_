b4b9a31f14edd425d7f3b02a4511481ec4125f9f
NadavShwartz93 <nadave.19933@gmail.com>
2022-02-26 11:56:29 +0200

    Update the dataset.cs script.

--- C#_Side_Project/runner.cs
+++ C#_Side_Project/runner.cs
@@ -3,21 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Assets.Scripts;
-
 namespace ConsoleApp
 {
     class runner
     {
-        public static Dataset dataset = Dataset.get_instance();
+        //public static Dataset dataset = Dataset.get_instance();
 
         public static void Main(string[] args)
         {
-            dataset.new_PatientDetailes("204110290", "right", 182, (float)0.5, "yes", 60);
-            dataset.new_data_line(1, (float)0.1, (float)0.9008626, (float)0.1519834, (float)0.02166149, 1, 51,
+            Dataset.get_instance().new_PatientDetailes("204110290", "right", 182, (float)0.5, "yes", 60);
+            Dataset.get_instance().new_data_line(1, (float)0.1, (float)0.9008626, (float)0.1519834, (float)0.02166149, 1, 51,
                 (float)0.8, (float)0.4, (float)0.5, "Front-Bottom-Center");
             //dataset.read_file("PatientResults.csv");
-            dataset.write_data_to_file();
+            Dataset.get_instance().write_data_to_file();
 
             Console.WriteLine("Process finished!\n");
         }

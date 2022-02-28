ff09377b11b86ef996a1300d67c160ee5161caf9
NadavShwartz93 <nadave.19933@gmail.com>
2022-02-28 18:20:26 +0200

    I Made minor updates.

--- C#_Side_Project/runner.cs
+++ C#_Side_Project/runner.cs
@@ -11,13 +11,13 @@ namespace ConsoleApp
 
         public static void Main(string[] args)
         {
-            Dataset.get_instance().new_PatientDetailes("204110290", "right", 182, (float)0.5, "yes", 60);
-            Dataset.get_instance().new_data_line(1, (float)0.1, (float)0.9008626, (float)0.1519834, (float)0.02166149, 1, 51,
+            Dataset.Get_instance().New_PatientDetailes("204110290", "right", 182, (float)0.5, "yes", 60);
+            Dataset.Get_instance().New_data_line(1, (float)0.1, (float)0.9008626, (float)0.1519834, (float)0.02166149, 1, 51,
                 (float)0.8, (float)0.4, (float)0.5, "Front-Bottom-Center");
             //dataset.read_file("PatientResults.csv");
-            Dataset.get_instance().write_data_to_file();
+            Dataset.Get_instance().Write_data_to_file();
             //Kmeans km = new Kmeans(numberColumns);
-            Kmeans.get_instance().read_file();
+            Kmeans.Get_instance().Read_file();
 
             Console.WriteLine("Process finished!\n");
             Console.ReadKey();
